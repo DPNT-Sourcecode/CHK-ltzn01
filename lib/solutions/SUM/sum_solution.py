080deaff7cb1dc236d0224927645ba57537ee87a
sbx_user1051 <sbx_user1051@169.254.99.189>
2023-10-23 20:56:06 +0000

    Mon Oct 23 20:56:06 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,19 @@
-# noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    return x * y
+def compute(x: int, y: int) -> int:
+    """
+    Adds two positive integers.
+
+    Args:
+        x (int): A positive integer between 0 and 100.
+        y (int): A positive integer between 0 and 100.
+
+    Returns:
+        int: The sum of x and y.
+
+    Raises:
+        ValueError: If x or y is not between 0 and 100 inclusive.
+    """
+    if not (0 <= x <= 100) or not (0 <= y <= 100):
+        raise ValueError("x and y must be between 0 and 100")
+    return x + y
+
 
