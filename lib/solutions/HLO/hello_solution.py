725c38f1e8fa8242059125f2d09d02a93a1b28d5
sbx_user1051 <sbx_user1051@169.254.99.189>
2023-10-23 21:04:06 +0000

    Mon Oct 23 21:04:06 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,6 +1,12 @@
+def hello(friend_name: str) -> str:
+    """
+    Returns a greeting message.
 
+    Args:
+        friend_name (str): A name to be included in the greeting.
+    
+    Returns:
+        str: A greeting message.
+    """
+    return "hello to the world!"
 
-# noinspection PyUnusedLocal
-# friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
