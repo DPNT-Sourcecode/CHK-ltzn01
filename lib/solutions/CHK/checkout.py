794cc46bbefc88684aeef7257599aaff281b6ead
sbx_user1051 <sbx_user1051@169.254.99.189>
2023-10-23 23:07:21 +0000

    Mon Oct 23 23:07:21 UTC 2023

--- lib/solutions/CHK/checkout.py
+++ lib/solutions/CHK/checkout.py
@@ -1,131 +1,17 @@
 from collections import Counter
-from product import Product, Offer
+# from product import Product, Offer
 
 class Checkout:
 
     def __init__(self, products):
-        """
-        Initailises a new checkout instance
+        pass
 
-        Args:
-            products (dict): A dictionary of products available for checkout
-        """
-        self.products = products
-        self.basket = {}
-    
-    def calculate_total(self, skus):
-        """
-        Calculates the total price of the items in the basket.
-        """
+class basket:
 
-
-
-
-
-
-# # Data-Oriented Class
-# class Basket:
-#     """
-#     A class representing a shopping basket.
-
-#     Attributes:
-#         items (Counter): A counter of the products in the basket
-#         total (float): The total price of the products in the basket.
-#     """
-
-#     def __init__(self):
-#         """Initialises a new empty basket."""
-#         self.items = Counter()
-#         self.total = 0
-
-#     def add(self, product: Product) -> None:
-#         """
-#         Add a product to a basket.
-
-#         Args:
-#             product (Product): The product to be added.
-#         """
-#         self.items[product.sku] += 1
-#         self.total += product.price
-
-
-# class Checkout:
-
-#     def __init__(self, products):
-#         self.products = products
-#         self.offer_applier = 
-
-
-# class OfferApplier(products)
-
-
-
-
-
-
-# # Behavior-Oriented Class - think about splitting up
-# class Checkout:
-#     """
-#     A class repesenting the checkout process
-#     """
-
-#     def __init__(self, products):
-#         """
-#         Initialises a new checkout with the given products.
-
-#         Args:
-#             products (dict of str: Product): A dictionary of products available.
-#         """
-#         self.products = products
-    
-#     def calculate_total(self, skus: str) -> int:
-#         """
-#         Calculate the total price of the products in the basket
-
-#         Args:
-#             skus (str): A string of SKUs representing the products in the basket.
-        
-#             Returns:
-#                 Int: The total price of the products in the basket. Returns -1 for any illegal input.
-#         """         
-#         basket = Basket()
-#         for sku in skus:
-#             if sku not in self.products:
-#                 return -1
-#             basket.add(self.products[sku])
-        
-#         for sku, count in basket.items.items():
-#             product = self.products[sku]
-#             for offer in sorted(product.offers, key=lambda o: -o.discount_value()):
-#                 remaining_count = self.apply_offer(basket, product, offer, count)
-#                 count = remaining_count
-        
-#         return int(basket.total)
-    
-#     def apply_offer(self, basket, product, offer, count):
-#         """
-#         Applies a special offer to the products in the basket.
-
-#         Args:
-#             basket (Basket): The shopping basket.
-#             product (Product): The product the offer applies to.
-#             offer (Offer): The special offer.
-#             count (int): the quantity of the product in the basket.
-        
-#         Returns:
-#             int: The remaining count of the product after applying offer
-#         """
-#         if offer.free_sku:
-#             free_product = self.products[offer.free_sku]
-#             free_count = count // offer.required_quantity
-#             basket.total -= free_count * free_product.price
-#             basket.items[free_product.sku] -= free_count
-#             return count
-#         # else:
-#         #     offer_count = count // offer.required_quantity
-
-
+    def __init__(self, products):
+        pass
     
 
                 
+
 
--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,5 @@
-from product import Product, Offer
-from checkout import Checkout
+# from product import Product, Offer
+# from checkout import Checkout
 
 def checkout(skus: str) -> int:
     """
@@ -11,48 +11,50 @@ def checkout(skus: str) -> int:
     Returns:
         int: The total checkout value of the items. Returns -1 for any illegal input.
     """
+    pass
 
-    # Think about putting these into some sort of JSON format?
-    # Define the products
-    product_a = Product('A', 50)
-    product_b = Product('B', 30)
-    product_c = Product('C', 20)
-    product_d = Product('D', 15)
-    product_e = Product('E', 40)
+    # # Think about putting these into some sort of JSON format?
+    # # Define the products
+    # product_a = Product('A', 50)
+    # product_b = Product('B', 30)
+    # product_c = Product('C', 20)
+    # product_d = Product('D', 15)
+    # product_e = Product('E', 40)
 
-    # Add Product A Offers
-    offer_a1 = Offer(3, 130)
-    offer_a2 = Offer(5, 200)
-    product_a.add_offer(offer_a1)
-    product_a.add_offer(offer_a2)
+    # # Add Product A Offers
+    # offer_a1 = Offer(3, 130)
+    # offer_a2 = Offer(5, 200)
+    # product_a.add_offer(offer_a1)
+    # product_a.add_offer(offer_a2)
 
-    # Add Product B Offers
-    offer_b1 = Offer(2, 45)
-    product_b.add_offer(offer_b1)
+    # # Add Product B Offers
+    # offer_b1 = Offer(2, 45)
+    # product_b.add_offer(offer_b1)
 
-    # Add Product E Offers
-    offer_e = Offer(2, 0, free_sku='B')
-    product_e.add_offer(offer_e)
+    # # Add Product E Offers
+    # offer_e = Offer(2, 0, free_sku='B')
+    # product_e.add_offer(offer_e)
 
 
-    # Define all products to checkout
-    products = {"A": product_a, 
-                "B": product_b,
-                "C": product_c,
-                "D": product_d,
-                "E": product_e
-    }
+    # # Define all products to checkout
+    # products = {"A": product_a, 
+    #             "B": product_b,
+    #             "C": product_c,
+    #             "D": product_d,
+    #             "E": product_e
+    # }
 
-    # Checkout
-    checkout_instance = Checkout(products)
+    # # Checkout
+    # checkout_instance = Checkout(products)
 
-    total = checkout_instance.calculate_total(skus)
+    # total = checkout_instance.calculate_total(skus)
 
-    return total
+    # return total
 
-assert checkout("A") == 50
-assert checkout("AAA") == 130
-assert checkout("EEB") == 80
-assert checkout("BEE") == 80
+# assert checkout("A") == 50
+# assert checkout("AAA") == 130
+# assert checkout("EEB") == 80
+# assert checkout("BEE") == 80
+
 
 
