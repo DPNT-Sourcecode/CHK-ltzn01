4190c8b33dc8fb3c79802c1d797f72727636f7c2
sbx_user1051 <sbx_user1051@169.254.99.189>
2023-10-23 21:29:54 +0000

    Mon Oct 23 21:29:54 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,25 @@
 
+def checkout(skus: str) -> int:
+    """
+    Calculate total price of a number of items.
+    """
+    # First validated the input
+    if not all(c in {'A', 'B', 'C', 'D'} for c in skus):
+        return -1
+    
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    offers = {'A': (3, 130), 'B': (2, 45)}
 
-# noinspection PyUnusedLocal
-# skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+    # Calculate number of each item 
+    counts = {}
+    for item in skus:
+        counts[item] = counts.get(item, 0) + 1
+    
+    total = 0
+    for item, count in counts.items():
+        price = prices[item]
+
+
+test_sku = "AAAB"
+checkout(test_sku)
+
