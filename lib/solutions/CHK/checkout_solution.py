2e837c182641938969f2561719d3f5e59144a5fa
sbx_user1051 <sbx_user1051@169.254.99.189>
2023-10-23 21:38:54 +0000

    Mon Oct 23 21:38:54 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,34 +9,27 @@ def checkout(skus: str) -> int:
     Returns:
         int: The total checkout value of the items. Returns -1 for any illegal input.
     """
-    # First validated the input
     if not all(c in {'A', 'B', 'C', 'D'} for c in skus):
         return -1
     
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     offers = {'A': (3, 130), 'B': (2, 45)}
 
-    # Calculate number of each item 
     counts = {}
     for item in skus:
         counts[item] = counts.get(item, 0) + 1
     
-    # Calculate total cost for each item
     total = 0
     for item, count in counts.items():
         price = prices[item]
-        total += count * price
-        # Figure out way for offers
         if item in offers:
             offer_quantity, offer_price = offers[item]
             offer_count = count // offer_quantity
             total += offer_count * offer_price
             count -= offer_count * offer_quantity
+        total += count * price
     return total
 
-
-test_sku = "AAAB"
-print(checkout(test_sku))
 
 
 
