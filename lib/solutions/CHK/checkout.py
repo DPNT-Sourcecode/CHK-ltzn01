b100b82e2ff9ec9372694b8c0b6135c749ae4dbf
sbx_user1051 <sbx_user1051@169.254.99.189>
2023-10-23 22:55:21 +0000

    Mon Oct 23 22:55:21 UTC 2023

--- lib/solutions/CHK/checkout.py
+++ lib/solutions/CHK/checkout.py
@@ -26,66 +26,66 @@ class Basket:
         self.items[product.sku] += 1
         self.total += product.price
 
-# Behavior-Oriented Class - think about splitting up
-class Checkout:
-    """
-    A class repesenting the checkout process
-    """
+# # Behavior-Oriented Class - think about splitting up
+# class Checkout:
+#     """
+#     A class repesenting the checkout process
+#     """
 
-    def __init__(self, products):
-        """
-        Initialises a new checkout with the given products.
+#     def __init__(self, products):
+#         """
+#         Initialises a new checkout with the given products.
 
-        Args:
-            products (dict of str: Product): A dictionary of products available.
-        """
-        self.products = products
+#         Args:
+#             products (dict of str: Product): A dictionary of products available.
+#         """
+#         self.products = products
     
-    def calculate_total(self, skus: str) -> int:
-        """
-        Calculate the total price of the products in the basket
+#     def calculate_total(self, skus: str) -> int:
+#         """
+#         Calculate the total price of the products in the basket
 
-        Args:
-            skus (str): A string of SKUs representing the products in the basket.
+#         Args:
+#             skus (str): A string of SKUs representing the products in the basket.
         
-            Returns:
-                Int: The total price of the products in the basket. Returns -1 for any illegal input.
-        """         
-        basket = Basket()
-        for sku in skus:
-            if sku not in self.products:
-                return -1
-            basket.add(self.products[sku])
+#             Returns:
+#                 Int: The total price of the products in the basket. Returns -1 for any illegal input.
+#         """         
+#         basket = Basket()
+#         for sku in skus:
+#             if sku not in self.products:
+#                 return -1
+#             basket.add(self.products[sku])
         
-        for sku, count in basket.items.items():
-            product = self.products[sku]
-            for offer in sorted(product.offers, key=lambda o: -o.discount_value()):
-                remaining_count = self.apply_offer(basket, product, offer, count)
-                count = remaining_count
+#         for sku, count in basket.items.items():
+#             product = self.products[sku]
+#             for offer in sorted(product.offers, key=lambda o: -o.discount_value()):
+#                 remaining_count = self.apply_offer(basket, product, offer, count)
+#                 count = remaining_count
         
-        return int(basket.total)
+#         return int(basket.total)
     
-    def apply_offer(self, basket, product, offer, count):
-        """
-        Applies a special offer to the products in the basket.
+#     def apply_offer(self, basket, product, offer, count):
+#         """
+#         Applies a special offer to the products in the basket.
 
-        Args:
-            basket (Basket): The shopping basket.
-            product (Product): The product the offer applies to.
-            offer (Offer): The special offer.
-            count (int): the quantity of the product in the basket.
+#         Args:
+#             basket (Basket): The shopping basket.
+#             product (Product): The product the offer applies to.
+#             offer (Offer): The special offer.
+#             count (int): the quantity of the product in the basket.
         
-        Returns:
-            int: The remaining count of the product after applying offer
-        """
-        if offer.free_sku:
-            free_product = self.products[offer.free_sku]
-            free_count = count // offer.required_quantity
-            basket.total -= free_count * free_product.price
-            basket.items[free_product.sku] -= free_count
-            return count
-        # else:
-        #     offer_count = count // offer.required_quantity
+#         Returns:
+#             int: The remaining count of the product after applying offer
+#         """
+#         if offer.free_sku:
+#             free_product = self.products[offer.free_sku]
+#             free_count = count // offer.required_quantity
+#             basket.total -= free_count * free_product.price
+#             basket.items[free_product.sku] -= free_count
+#             return count
+#         # else:
+#         #     offer_count = count // offer.required_quantity
 
 
     
