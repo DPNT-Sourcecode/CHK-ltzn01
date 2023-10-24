071a21e0a5bd1c13c0123451f4f89b9dfad310ef
sbx_user1051 <sbx_user1051@169.254.15.85>
2023-10-24 17:34:44 +0000

    Tue Oct 24 17:34:44 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
 from typing import List, Optional
 import copy
+
 # File imports are not working for Accelerate Runner
 # All code will have to be on this single file
-
-# from items import Item, Basket
 
 ####################################################################################################
 ############################################### items.py ###########################################
@@ -72,7 +72,134 @@ class Basket:
             print(product.sku, product.price)
 
 
+####################################################################################################
+########################################### discounts.py ###########################################
+####################################################################################################
+
+class DiscountStrategy(ABC):
+    """
+    Abstract base class for various discount strategies
+
+    Attributes:
+        item: The item that the discount applies to.
+        trigger_quantity: The quantity of items required to trigger the discount.
+        bulk_price: The price of the item when the discount is triggered
+    """
+
+    @abstractmethod
+    def is_applicable(self, basket: 'Basket') -> bool:
+        """
+        Method to check if the discount can be applied to the basket
+        """
+        pass
+
+    @abstractmethod
+    def apply_discount(self, basket: 'Basket') -> None:
+        """
+        Method to apply the discount to the basket
+        """
+        pass
+
+class BulkDiscount(DiscountStrategy):
+    """
+    Applies a bulk discount based on specific quantity
+
+    Attributes:
+        item: The item that the discount applies to.
+        trigger_quantity: The quantity of items required to trigger the discount.
+        Bulk_price: The price of the item when the discount is triggered.
+    """
+
+    def __init__(self, item: Item, trigger_quantity: int, bulk_price: float):
+        self.item = item
+        self.trigger_quantity = trigger_quantity
+        self.bulk_price = bulk_price
     
+    def is_applicable(self, basket: Basket) -> bool:
+        """
+        Check if the bulk purchase discount is applicable.
+
+        Args:
+            basket: The basket to check for applicability.
+        
+        Returns:
+            A boolean indicating whether the discount is applicable
+        """
+        trigger_count = 0
+        for product in basket.products:
+            if product.sku == self.item.sku and not product.discounted:
+                trigger_count += 1
+        return trigger_count >= self.trigger_quantity
+    
+    def apply_discount(self, basket: Basket) -> None:
+        """
+        Apply the bulk purchase discount.
+
+        Args:
+            basket: The basket to apply the discount to
+        """
+        num_changed = 0
+        if self.is_applicable(basket):
+            for product in basket.products:
+                if product.sku == self.item.sku and not product.discounted and num_changed < self.trigger_quantity:
+                    product.discounted_price = self.bulk_price / self.trigger_quantity
+                    product.discounted = True
+                    num_changed += 1
+
+
+class FreeItemDiscount(DiscountStrategy):
+    """
+    Discount strategy for applying a buy x get y free discount.
+
+    Attributes:
+        trigger_item: The item that triggers the discount.
+        trigger_quantity: The quantity of trigger items required to trigger the discount.
+        discounted_item: THe item that is discounted.
+    """
+
+    def __init__(self, trigger_item: Item, trigger_quantity: int, discounted_item: Item):
+        """
+        Initialise the free item discount.
+
+        Args:
+            trigger_item: The item that triggers the discount.
+            trigger_quantity: The quantity of trigger items required to trigger the discount.
+            discounted_item: The item that is discounted.
+        """
+        self.trigger_item = trigger_item
+        self.trigger_quantity = trigger_quantity
+        self.discounted_item = discounted_item
+    
+    def is_applicable(self, basket: Basket) -> bool:
+        """
+        Check if the free item discount is applicable.
+
+        Args:
+            basket: The basket to check for applicability
+        """
+        trigger_count = 0
+        for product in basket.products:
+            if product.sku == self.trigger_item.sku and not product.discounted:
+                trigger_count += 1
+        return trigger_count >= self.trigger_quantity
+    
+    def apply_discount(self, basket: Basket) -> None:
+        if self.is_applicable(basket):
+            trigger_count = 0
+            for product in basket.products:
+                if product.sku == self.trigger_item.sku and not product.discounted:
+                    product.discounted = True
+                    trigger_count += 1
+                    if trigger_count == self.trigger_quantity:
+                        break
+            # If the product is already in the basket, we don't want to add it again
+            # but we do want to update the price. It must not have been discounted before.
+            for product
+
+
+####################################################################################################
+######################################## checkout_solution.py ######################################
+####################################################################################################
 
 
 def checkout(skus: str) -> int:
@@ -85,16 +212,17 @@ def checkout(skus: str) -> int:
     Returns:
         int: The total checkout value of the items. Returns -1 for any illegal input.
     """
+    # Define the Products
     item_a = Item('A', 50)
-    print(item_a)
+    item_b = Item('B', 30)
+    item_c = Item('C', 20)
+    item_d = Item('D', 15)
+    item_e = Item('E', 40)
 
-    # # Think about putting these into some sort of JSON format?
-    # # Define the products
-    # product_a = Product('A', 50)
-    # product_b = Product('B', 30)
-    # product_c = Product('C', 20)
-    # product_d = Product('D', 15)
-    # product_e = Product('E', 40)
+    # Initialise the basket
+    basket = Basket()
+
+
 
     # # Add Product A Offers
     # offer_a1 = Offer(3, 130)
@@ -126,5 +254,6 @@ def checkout(skus: str) -> int:
 
     # return total
 print(checkout("A"))
+
 
 
