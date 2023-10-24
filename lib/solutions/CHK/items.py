35ef22b317a48231da493fc02e26d64040f4095f
sbx_user1051 <sbx_user1051@169.254.15.85>
2023-10-24 17:07:44 +0000

    Tue Oct 24 17:07:44 UTC 2023

---
+++ lib/solutions/CHK/items.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+import copy
+
+@dataclass
+class Item:
+    """
+    Represents an item in a shopping basket.
+
+    Attributes:
+        sku: The item's SKU.
+        price: The item's price.
+        discounted_price: The Item's discounted price
+        discounted: A boolean indicating whether the item has been discounted.
+    """
+    sku: str
+    price: float
+    discounted_price: Optional[float] = None
+    discounted: bool = False
+
+    def __post_init__(self):
+        """
+        Post-initialisation to set discounted price to the orginal price if not provided.
+        """
+        if self.discounted_price is None:
+            self.discounted_price = self.price
+
+
+@dataclass
+class Basket:
+    """
+    A class representing a shopping basket.
+
+    Attributes:
+        products: a list of products in the basket.
+    """
+    products: List[Item] = field(default_factory=list)
+
+    def add_product(self, product: Item):
+        """
+        Add a product to the basket.
+
+        Args:
+            product: The product to add to the basket
+        """
+        product_copy = copy.copy(product)
+        self.products.append(product_copy)
+
+
+    
